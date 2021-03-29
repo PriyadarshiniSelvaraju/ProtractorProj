52d0d8ae95b239f45b121bbaac64273ef8d6cfcb
Priya <priyadarshini.selvaraju@ge.com>
2021-03-29 15:08:36 -0700

    first commit

---
+++ conf.js
@@ -0,0 +1,25 @@
+var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
+
+exports.config = {
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['calcu.js'],
+
+    //execute as a prerequsite to all test cases
+    onPrepare : function ()
+    {
+        browser.driver.manage().window().maximize();
+        jasmine.getEnv().addReporter(
+            new Jasmine2HtmlReporter({
+                savePath: 'target/screenshots'
+            })
+        );
+    },
+    suites:
+        {
+            smoke: 'calcu.js'
+        },
+        jasmineNodeOpts:{
+            showColors:true,
+        }
+};
+
